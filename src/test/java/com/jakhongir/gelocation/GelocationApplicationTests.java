bee0ed2f057a14c8680940b6b690a4b5905d5ab5
JakhongirEngineer <jakhongirrasulovengineer@gmail.com>
2025-07-04 17:36:47 +0500

    create spring boot project

---
+++ src/test/java/com/jakhongir/gelocation/GelocationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jakhongir.gelocation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GelocationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
