5a9f16f39b9e4019b24f106ffa716087eb5c0ec3
JakhongirEngineer <jakhongirrasulovengineer@gmail.com>
2025-07-05 18:06:29 +0500

    add ip v4  validator

---
+++ src/main/java/com/jakhongir/gelocation/util/IPv4ValidatorRegex.java
@@ -0,0 +1,17 @@
+package com.jakhongir.gelocation.util;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+// source: https://mkyong.com/regular-expressions/how-to-validate-ip-address-with-regular-expression/
+public class IPv4ValidatorRegex {
+    private static final String IPV4_PATTERN =
+            "^(([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])(\\.(?!$)|$)){4}$";
+    private static final Pattern pattern = Pattern.compile(IPV4_PATTERN);
+
+    public static boolean isValid(final String email) {
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+}
